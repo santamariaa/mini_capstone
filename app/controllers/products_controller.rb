600950791f87e0eeb724681933110f3e5b6527eb
santamariaa <asantamaria101@gmail.com>
2016-01-17 16:58:13 -0600

    first_commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,46 @@
 class ProductsController < ApplicationController
-    def consumer_data
+    def index
         @products = Product.all
+    end
+
+    def show
+        @product = Product.find_by(id: params[:id])
+    end
+
+    def new
+    end
+
+    def create
+        @product = Product.create({name: params[:name],
+                                price: params[:price],
+                                image: params[:image],
+                                description: params[:description]})
 
     end
 
+    def edit
+        @product = Product.find_by(id: params[:id])
+    end
+
+    def update
+        @product = Product.find_by(id: params[:id])
+
+        @product.update({
+                       name: params[:name],
+                       price: params[:price],
+                       image: params[:image],
+                       description: params[:description]})
+
+    end
+
+    def destroy
+        @product = Product.find(params[:id])
+        @product.destroy
+    end
+
 end
+
+
+
+
+
